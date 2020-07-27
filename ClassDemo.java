fbde6f1227b336198e1d43dec8ffd4edc8319a0d
trainer2edureka <trainer2edureka@gmail.com>
2020-07-27 05:51:21 +0000

    mulitple files to be added

---
+++ ClassDemo.java
@@ -0,0 +1,15 @@
+package Module2;
+
+class ClassInfo{
+	int rollno;  
+	String name;  
+	 
+	
+ClassInfo(int rollno,String name){  
+this.rollno=rollno;  
+this.name=name;  
+  
+	}  
+	void display(){System.out.println(rollno+" "+name);}  
+	}  
+	
---
+++ ThisDemo.java
@@ -0,0 +1,12 @@
+package Module2;
+
+public class ThisDemo {
+
+	public static void main(String[] args) {
+		ClassInfo c1=new ClassInfo(10,"John");  
+		ClassInfo c2=new ClassInfo(12,"Annie");  
+		c1.display();  
+		c2.display(); 
+
+	}
+}
